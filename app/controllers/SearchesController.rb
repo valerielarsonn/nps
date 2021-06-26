63d1c0a662bcee2a62d8eabba7472a2367700eb5
valerielarsonn <valerielarsonn@gmail.com>
2021-06-26 11:50:21 -0600

    Fix NPS API connection

--- app/controllers/SearchesController.rb
+++ app/controllers/SearchesController.rb
@@ -1,5 +1,11 @@
 class SearchesController < ApplicationController
 
+    def nps_connect
+        @resp = Faraday.post('https://nps-projectfour.herokuapp.com/nps') do |req|
+        end
+        nps_search
+    end
+    
     def nps_search
         @search = Faraday.get('https://developer.nps.gov/api/v1/parks?&api_key=cdgjmt09KDFy4NRLi7OqQ02jhN37uSne53dnzURZ') do |req|
         end
